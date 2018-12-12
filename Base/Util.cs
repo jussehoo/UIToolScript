91c70949cc7fee7c83c0b8555e9de8319d34f878
jussehoo <jussehoo@github.com>
2018-12-12 19:57:41 +0200

    Utils.

---
+++ Base/Util.cs
@@ -0,0 +1,10 @@
+﻿public static class ArrayExtensions
+{
+	public static void Fill<T>(this T[] originalArray, T with)
+	{
+		for (int i = 0; i < originalArray.Length; i++)
+		{
+			originalArray[i] = with;
+		}
+	}
+}
