dc2997f52afe9025b9939d5b0228b758783dc9a9
jussehoo <jussehoo@github.com>
2019-01-19 20:32:43 +0200

    Clip: scaling delay.

--- UI/Sequence.cs
+++ UI/Sequence.cs
@@ -9,11 +9,6 @@ public class Sequence : MonoBehaviour
 	// TODO: ending condition (Action, time ending by delfault)
 	// TODO: simultaneous clips (IClip [little]sibling as a member)
 
-	public enum Type
-	{
-		ROTATE_TOWARDS,
-		MOVE_TO
-	}
 	private abstract class IClip
 	{
 		protected Sequence seq;
@@ -81,19 +76,26 @@ public class Sequence : MonoBehaviour
 	}
 	private class LocalScaling : IClip
 	{
-		float start, end, velocity, currentScale;
-		public LocalScaling(Sequence seq, float _from, float _to, float time) : base(seq, time)
+		float start, end, velocity, currentScale, delay;
+		public LocalScaling(Sequence seq, float _from, float _to, float time, float _delay = 0f) : base(seq, time)
 		{
 			start = _from;
 			end = _to;
+			delay = _delay;
 		}
 		public override void Init()
 		{
 			velocity = (end - start) / timeLeft;
 			currentScale = start;
+			seq.transform.localScale = new Vector3(currentScale, currentScale, currentScale);
 		}
 		public override bool Step()
 		{
+			if (delay > 0f)
+			{
+				delay -= Time.deltaTime;
+				return true;
+			}
 			if (UpdateTimer())
 			{
 				// update position
@@ -146,9 +148,9 @@ public class Sequence : MonoBehaviour
 	{
 		list.AddLast(new MoveTo(this, targetPosition, time));
 	}
-	public void AddLocalScaling(float from, float to, float time)
+	public void AddLocalScaling(float from, float to, float time, float delay = 0f)
 	{
-		list.AddLast(new LocalScaling(this, from, to, time));
+		list.AddLast(new LocalScaling(this, from, to, time, delay));
 	}
 	public void AddImageColorLerp(Image image, Color from, Color to, float time)
 	{
